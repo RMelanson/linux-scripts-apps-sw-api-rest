10d355ef936a4d51650767d0ba5f13711bbfa898
Robin Melanson <remcogold@gmail.com>
2018-05-16 01:12:02 -0400

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-currDir=$PWD
-swDir=/tmp/scripts/api/rest/stockWidgets
-alias cdsw='cd $swDir'
-git clone https://github.com/RMelanson/linux-scripts-api-rest-stockWidgets.git $swDir
-cd $swDir
-./setup.sh
-cd $currDir
