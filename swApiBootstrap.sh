5da7dfe5fd8c3984aebb47fffe9b18721dcd76cf
Robin Melanson <remcogold@gmail.com>
2018-05-19 12:55:55 -0400

    Create swApiBootstrap.sh

---
+++ swApiBootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+swRestDir=$PWD
+
+#Set Cloning Properties
+pkg=Web
+gitRepo="linux-scripts-apps-sw-api-rest"
+installDir="/tmp/scripts/apps/SW/API/REST"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $swRestDir
