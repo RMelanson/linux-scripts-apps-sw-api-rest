8ac300b0f66aab5719e55e2dffca3a8d5cd6fea9
Robin Melanson <remcogold@gmail.com>
2018-05-28 13:05:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@
 . ./env/setEnv.sh#
 
 # ADD STOCK WIDGETS ADMIN USER
-user=swadmin
-group=swadmin
-home=/home/SW_HOME
-pkg=stockWidgets
 ./install/addPkgUser.sh $user $group $home $pkg
 
 # INSTALL JBOSS
