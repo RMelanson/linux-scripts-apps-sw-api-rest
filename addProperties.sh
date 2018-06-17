570b9cdd79cfb53f21655e1f63c3c38c49d6d46d
Robin Melanson <remcogold@gmail.com>
2018-06-17 02:08:05 -0400

    Create addProperties.sh

---
+++ addProperties.sh
@@ -0,0 +1,21 @@
+dir=/opt/wildfly/standalone/configuration
+file=$dir/standalone.xml
+ext='extensions'
+props='system-properties'
+
+#cp $file.BAK $file
+if grep -Fq "$props"  $file; then
+  echo Properties $props found in $file
+  sed -i '/SW_HOME/d' $file
+  sed -i '/SW_LOG_HOME/d' $file
+  sed -i 's/<\/system-properties>/    <property name="SW_HOME" value=\/home\/SW_HOME"\/>\
+        <property name="SW_LOG_HOME" value="\/opt\/wildfly\/standalone\/log"\/>\
+    <\/system-properties>/' $file
+else
+  echo Extension $ext found in $file
+  sed -i 's/<\/extensions>/<\/extensions>\n\
+    <system-properties>\
+        <property name="SW_HOME" value=\/home\/SW_HOME"\/>\
+        <property name="SW_LOG_HOME" value="\/opt\/wildfly\/standalone\/log"\/>\
+    <\/system-properties>/' $file
+fi
