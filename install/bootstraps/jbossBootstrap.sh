493d50515e9c8e654343eec0b967ddf3740a3424
Robin Melanson <remcogold@gmail.com>
2018-06-17 03:06:41 -0400

    Update jbossBootstrap.sh

--- install/bootstraps/jbossBootstrap.sh
+++ install/bootstraps/jbossBootstrap.sh
@@ -1,26 +1,26 @@
-!/bin/bash
+#!/bin/bash
 jbossBootCurrDir=$PWD
 
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root or under sudo"
-  exit -1
+  return -1
 fi
 
-if [ -z "$1" ]; then
-   branch=master
-else
-   branch=$1
-fi
+#INITIAL BASIC TOOLS INSTALL
+yum update -y
+
+#INSTALL GIT
+yum install git -y
 
 #Set Cloning Properties
 pkg=JBOSS
 gitRepo="linux-scripts-apps-jboss.git $jbossDir"
 installDir="/tmp/scripts/apps/JBOSS"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone -b $branch git@github.com:RMelanson/"
+   clone="git clone git@github.com:RMelanson/"
 else
-   clone="git clone -b $branch https://github.com/RMelanson/"
+   clone="git clone https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
