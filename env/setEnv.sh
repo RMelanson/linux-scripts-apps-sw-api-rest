1e08433b892015345ffab786d981abe73bcb65b3
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:50:03 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # STOCK WIDGETS CONFIGURATION PARAMETERSexport PKG=SW
+pkg=STOCK_WIDGETS
+baseDir=/tmp/scripts
+subDir=apps
+installDir="$baseDir/$subDir/$subDir/REST"
+gitRepo="linux-scripts-apps-sw-api-rest"
+
 export SW_INSTALL=$PWD
 export SW_HOME=/home/SW_HOME
 SW_ENV=$SW_HOME/.env
-mkdir $SW_HOME
-mkdir $SW_ENV
-#cp -raf $SW_INSTALL/env/* $SW_ENV
-#. $SW_ENV/.alias
 
 wfDeployDir=/opt/wildfly/standalone/deployments
 user=swadmin
 group=swadmin
 home=/home/SW_HOME
-pkg=stockWidgets
 
-baseDir=/tmp/scripts
-subDir=apps
 instpkg=SW/API/REST
-installDir="$baseDir/$subDirSW/API/REST"
-gitRepo="linux-scripts-apps-sw-api-rest"
