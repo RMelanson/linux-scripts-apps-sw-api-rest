d499d763e5f9e0b6d25f984cb0d589912771dbe5
Robin Melanson <remcogold@gmail.com>
2019-03-17 01:18:24 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# Ensure script is running under root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root or under sudo"
-  exit -1
-fi
-
-#INITIAL BASIC TOOLS INSTALL
-yum update -y
-
-#INSTALL GIT
-yum install git -y
-
 # STOCK WIDGETS CONFIGURATION PARAMETERSexport PKG=SW
-pkg=STOCK_WIDGETS
+pkg=SW_API_REST
+gitRepo="linux-scripts-apps-sw-api-rest"
+installDir="/tmp/scripts/apps/$pkg"
 baseDir=/tmp/scripts
 subDir=apps
-installDir="$baseDir/$subDir/$subDir/REST"
-gitRepo="linux-scripts-apps-sw-api-rest"
 
 export SW_INSTALL=$PWD
 export SW_HOME=/home/SW_HOME
