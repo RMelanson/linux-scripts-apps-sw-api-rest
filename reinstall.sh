a7091ac469a9753f6349b4f000afdc1e2bdb4487
Robin Melanson <remcogold@gmail.com>
2019-04-22 15:52:16 -0400

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
-# INSTALL JENKINS ON LINUX WITH REMOTE ACCESS AS A SERVICE
 
 # SETUP WILDFLY CONFIGURATION ENVIRONMENT AND PARAMETERS
 clear
 echo reinstall.sh:  EXECUTING ENVIRONMENT SETUP: . ./env/setEnv.sh
 . ./env/setEnv.sh
 
-echo reinstall.sh:  EXECUTING cp $bootstrap ..
-cp $bootstrap ..
-cd ..
+echo reinstall.sh:  EXECUTING cp $bootstrap $parentDir
+cp $bootstrap $parentDir
+cd $parentDir
 
 echo reinstall.sh:  EXECUTING REMOVE OLD INSTALLATION: rm -rf $installDir
 rm -rf $installDir
 
-echo reinstall.sh:  EXECUTING BOOTSTRAP: ./$bootstrap
-./$bootstrap
+echo reinstall.sh:  EXECUTING BOOTSTRAP: . ./$bootstrap
+. ./$bootstrap
 
 echo reinstall.sh:  EXECUTING BOOTSTRAP: rm $bootstrap
 rm $bootstrap
