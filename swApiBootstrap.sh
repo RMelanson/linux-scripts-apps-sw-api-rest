654ec741a26aa8ba2c8e7c96e8df1a588ba9fb0e
Robin Melanson <remcogold@gmail.com>
2019-05-07 14:22:08 -0400

    Update swApiBootstrap.sh

--- swApiBootstrap.sh
+++ swApiBootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+bootstrapDir=$PWD
+bootstrap=$bootstrapDir/ZZZZZZZZZZZZZZZZ.sh
 
 # Ensure script is running under root
-bootstrap=$webCurrDir/swApiBootstrap.sh
 if [ "$EUID" -ne 0 ]
 then
    sudo -n true 2/dev/null 2>&1
